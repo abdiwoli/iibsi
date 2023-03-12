d9c7e28bdf93409cd1fb587c6879e3029a0af111
abdiwoli <abdiwolih@gmail.com>
2023-03-12 11:19:44 +0300

    update

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 (function(){
-  angular.module("firstApp",[])
+  angular.module("iibsi",[])
   .controller('main',Main);
   Main.$inject = ['$scope','$timeout'];
   function Main($scope,$timeout){
-    $scope.counter =0;
-    $scope.upCounter = function(){
-      $timeout(function(){
-        $scope.counter++;
-      },2000);
-    }
+    var images = [
+      "/pics/home.jfif", "/pics/land.jfif", "/pics/computers.jfif", "/pics/sofa.jfif", "/pics/food.jfif"];
+    var titles = [
+      "Buy new Home", "invest in land", "buy new Computer", "refresh your home","home delevery"];
+    $scope.count = 1;
+    $scope.title = titles[$scope.count];
+    $scope.img = images[$scope.count];
+
+     var timer= $timeout(function interval(){
+       $scope.count = $scope.count%(images.length)+1;
+       $scope.title = titles[$scope.count-1];
+       $scope.img = images[$scope.count-1];
+      timer = $timeout(interval, 2000);
+     },2000)
+
+
+
   }
 })();
